baebb88235c7ebefbc0fd1778cd92b91ac1aeba0
K.D.W <kadendw23@gmail.com>
2025-08-23 15:10:13 -0500

    Main Menu
    - Progress: 90% Complete
    - Remaining: Volume/SFX Bar

--- Assets/Scripts/Menus/Main Menu.cs
+++ Assets/Scripts/Menus/Main Menu.cs
@@ -1,16 +1,21 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void Play()
     {
-        
+        SceneManager.LoadScene("Test Scene");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Options()
     {
-        
+        Debug.Log("Options menu opened");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("You've quit the game!");
     }
 }
