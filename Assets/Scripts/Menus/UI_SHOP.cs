069996498821d77a69f75251328d8b555ecf3242
K.D.W <kadendw23@gmail.com>
2025-08-26 21:09:39 -0500

    W.I.P Upgrade Shop

---
+++ Assets/Scripts/Menus/UI_SHOP.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class UI_SHOP : MonoBehaviour
+{
+    private Transform container;
+    private Transform shopItemTemplate;
+
+    private void Awake()
+    {
+        container = transform.Find("container");
+        shopItemTemplate = container.Find("shopItemTemplate");
+        shopItemTemplate.gameObject.SetActive(false);
+    }
+}
