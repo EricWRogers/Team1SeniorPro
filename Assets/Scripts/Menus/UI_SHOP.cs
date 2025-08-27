cfd427e0ecd59a6ba793b5350f793f8355622907
K.D.W <kadendw23@gmail.com>
2025-08-27 17:32:24 -0500

    Shop Implemented
    - Player SHOULD be able to "purchase" an upgrade with the right amount of pigment collected.

--- Assets/Scripts/Menus/UI_SHOP.cs
+++ Assets/Scripts/Menus/UI_SHOP.cs
@@ -1,14 +1,40 @@
 using UnityEngine;
-
 public class UI_SHOP : MonoBehaviour
 {
     private Transform container;
     private Transform shopItemTemplate;
+    private PlayerCurrency playerCurrency;
 
+    public GameObject UPG;
+    public GameObject UPG2;
+    public GameObject UPGMAX;
     private void Awake()
     {
         container = transform.Find("container");
-        shopItemTemplate = container.Find("shopItemTemplate");
-        shopItemTemplate.gameObject.SetActive(false);
+        shopItemTemplate = container.Find("shopUpgTemplate");
+        shopItemTemplate.gameObject.SetActive(true);
+
+        playerCurrency = Object.FindFirstObjectByType<PlayerCurrency>();
+    }
+
+    public void Upgrade()
+    {
+        if (playerCurrency == null) return;
+
+        if (playerCurrency.pigment >= 10)
+        {
+            UPG.SetActive(true);
+        }
+
+        if (playerCurrency.pigment >= 50)
+        {
+            UPG2.SetActive(true);
+        }
+
+        if (playerCurrency.pigment >= 100)
+        {
+            UPGMAX.SetActive(true);
+        }
+        
     }
 }
